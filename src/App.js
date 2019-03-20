152d8e92748b6264ac0abbe4ca1838af03fe554c
Stiven <sduque866@gmail.com>
2019-03-20 17:42:55 -0500

    prueba #2

--- src/App.js
+++ src/App.js
@@ -2,16 +2,71 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from  './Person/Person';
 
+
 class App extends Component {
+  state ={
+    persons: [
+      {name:'Stiven', age:20},
+      {name:'Andres', age:67},
+      {name:'Juan', age:6}
+    ],
+    otherState:'some other value'
+  }
+
+  handleSwitchName = (newName)=>{
+    //console.log('Was clicked!')
+    this.setState(
+      {
+        persons: [
+          {name:newName, age:29},
+          {name:'Juan Andres', age:6},
+          {name:'Juan', age:66}
+        ]
+      }
+    );
+  }
+
+  handleNameChange = (event) =>{
+    this.setState(
+      {
+        persons: [
+          {name:'Cuchito', age:29},
+          {name:event.target.value, age:6},
+          {name:'Juan', age:0}
+        ]
+      }
+    );
+  }
+
   render() {
+    const style = {
+      backgroundColor:'white',
+      font:'inherit',
+      border:'1px solid blue',
+      padding:'8px',
+      cursor:'pointer'
+    };
     return (
       <div className="App">
         <h1>hellokajshdjha</h1>
-        <Person/>
+        <button 
+          style={style}
+          onClick={()=>this.handleSwitchName("jajaja")}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={()=>this.handleSwitchName("jejejeej")}
+          changed={this.handleNameChange}>Hello j:ajsdkjashd</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}
+          />
       </div>
     );
-    
-
   }
 }
 
---
+++ src/users/Users.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+class Users extends Comment{
+    render(){
+        return(
+            
+        );
+    }
+
+};
+
+export default Users;
